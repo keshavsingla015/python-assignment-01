b597bde8d6a87291420519667120fd78a991f2ad
keshavsingla015 <97163964+keshavsingla015@users.noreply.github.com>
2022-01-09 15:45:24 +0530

    Add files via upload

---
+++ output_21103018.py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\KESHAV SINGLA\Desktop\ques1 .py ==============
+enter first number: 23
+enter second number: 21
+enter third number: 10
+47.333333333333336
+
+
+
+=============== RESTART: C:\Users\KESHAV SINGLA\Desktop\ques2.py ===============
+enter your income:$ 150000
+number of dependents on you: 2
+your payable tax is $14000.0
+
+
+
+
+=============== RESTART: C:\Users\KESHAV SINGLA\Desktop\ques3.py ===============
+what is your name: Keshav Singla
+put down your sid: 21103018
+tell your CGPA:9.7
+Gender: (M/F/U) M
+Under which course: cse
+['name', 'sid', 'cgpa', 'gender', 'course_name']
+['Keshav Singla', 21103018, 9.7, 'M', 'cse']
+
+
+
+
+=============== RESTART: C:\Users\KESHAV SINGLA\Desktop\ques4.py ===============
+enter marks of first student: 23
+enter marks of second student: 25
+enter marks of third student: 19
+enter marks of 4th student: 15
+enter marks of 5th student: 25
+[15, 25, 19, 25, 23]
+
+Marks in Ascending Order
+[15, 19, 23, 25, 25]
+
+Marks in Decreasing order
+[25, 25, 23, 19, 15]
+
+
+
+
+=============== RESTART: C:\Users\KESHAV SINGLA\Desktop\ques5.py ===============
+A.
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+PART B
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
+
+
+
